ad8afca922d2309f0ab1b5d983d3d28d90b7c5f9
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-08 13:01:20 +0200

    Implemented throttle logic into custom hook

---
+++ src/hooks/useThrottled.ts
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+
+const DEFAULT_THROTTLE_DELAY = 1000;
+
+export const useThrottled = <T>(value: T, delay = DEFAULT_THROTTLE_DELAY) => {
+  const [throttledValue, setThrottledValue] = useState<T>(value);
+  const lastUpdatedTime = useRef<ReturnType<typeof Date.now>>(Date.now());
+
+  function getTimeRemaining(throttleDelay: number) {
+    const elapsedTime = Date.now() - lastUpdatedTime.current;
+    return elapsedTime < throttleDelay ? throttleDelay - elapsedTime : 0;
+  }
+
+  useEffect(() => {
+    const timeRemaining = getTimeRemaining(delay);
+
+    if (timeRemaining === 0) {
+      setThrottledValue(value);
+      lastUpdatedTime.current = Date.now();
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setThrottledValue(value);
+      lastUpdatedTime.current = Date.now();
+    }, timeRemaining);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return throttledValue;
+};
